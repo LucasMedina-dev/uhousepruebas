b2fb1f7a427547164dde2eb039840d7278855275
LucasMedina-dev <medinalucasian@gmail.com>
2021-10-28 00:19:21 -0300

    Update busqueda.js

--- js/busqueda.js
+++ js/busqueda.js
@@ -197,7 +197,7 @@ fetch("../js/db.json")
                 $(this).children(".propiedades_favorito").removeClass("propiedades_favorito-true")
                 $(this).children(".propiedades_favorito").addClass("propiedades_favorito-false")
             }
-        })
+        }))
         $(".propiedades_toggle").click(function(){ 
             $(this).fadeOut(500)
             $(this).parent().children(".propiedades_informacion").delay(500)
