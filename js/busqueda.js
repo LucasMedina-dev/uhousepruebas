cfcfe1cd333583b6f25b61118c5c6f7aab2cea95
LucasMedina-dev <medinalucasian@gmail.com>
2021-10-31 03:05:25 -0300

    Update busqueda.js

--- js/busqueda.js
+++ js/busqueda.js
@@ -47,7 +47,7 @@ fetch("../js/db.json")
     .then(
     (data) => {
         let tipo= ["casa", "ph"]
-        domicilios= tipo.forEach(y => data.filter(x => x.tipo===y))
+        domicilios= data.filter(x => x.tipo==="casa")
 
         let final= domicilios.filter(x => x.ciudad.toLowerCase()===ciudad && x.precio>precioMinimo && x.precio<precioMaximo)
 
