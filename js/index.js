69e6a86c384f8a4162678fa0302ba105fe9bda7c
LucasMedina-dev <medinalucasian@gmail.com>
2021-11-01 20:45:48 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,5 +31,5 @@ $(".header_boton").click(function(){
 $("#boton").click(()=>{
     $(".false").toggle(300)
 })
-tipos=["casa", "departamentos", "ph"]
+tipos=["casa", "departamento", "ph"]
 guardarLS("tipos", JSON.stringify(tipos))
