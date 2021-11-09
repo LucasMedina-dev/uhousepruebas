d84531e6b79f6e172fa4f6b01c31c857e81cc2a3
LucasMedina-dev <medinalucasian@gmail.com>
2021-11-09 20:49:19 -0300

    asd

--- js/funciones.js
+++ js/funciones.js
@@ -58,7 +58,7 @@ function comprobarResultado(){
     }
 }
 function obtenerTipos(){
-    if (localStorage.getItem("tipos")===null){
+    if (localStorage.getItem("tipos")===null || tipos.length===0){
         let tipos=["casa","departamento","ph"]
     }else{
         let tipos= JSON.parse(localStorage.getItem("tipos")) 
