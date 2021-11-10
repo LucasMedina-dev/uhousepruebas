258c47510a7c6df15028cd07b9333d2a2f6603d7
LucasMedina-dev <medinalucasian@gmail.com>
2021-11-09 21:03:51 -0300

    asd

--- js/busqueda.js
+++ js/busqueda.js
@@ -70,7 +70,6 @@ fetch("../js/db.json")
     .then((response) => response.json())
     .then(
     (data) => {
-        obtenerTipos()
         tipos.forEach(y=> domicilios= domicilios.concat(data.filter(x => x.tipo===y)))
         // "tipos" son los tipos de propiedades(casa,dpto,ph), se filtran las propiedades que coincidan con la busqueda
         let final= domicilios.filter(x => x.ciudad.toLowerCase()===ciudad && x.precio>=precioMinimo && x.precio<=precioMaximo)
--- js/funciones.js
+++ js/funciones.js
@@ -65,3 +65,4 @@ function obtenerTipos(){
     }
     guardarLS("tipos", JSON.stringify(tipos))
 }
+obtenerTipos()
